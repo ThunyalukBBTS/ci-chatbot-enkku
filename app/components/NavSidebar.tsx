14b99c7264b0331745860a8f4a958fd6a0bb4c09
thunyalukBBTS <thunyaluk.sa@kkumail.com>
2025-03-29 23:14:27 +0700

    [Add] - Update ui and test integrate with local engine

---
+++ app/components/NavSidebar.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function NavSidebar() {
+    const router = useRouter();
+
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
+    return (
+        <div className="w-ful">
+            {/* Sidebar */}
+            <div
+                className={`bg-orange-700 text-white md:w-100 w-screen p-2 fixed left-0 top-0 h-full z-10 transition-transform duration-300 ${isSidebarOpen ? "translate-y-0" : "-translate-y-full"
+                    }`}
+            >
+                <div className="flex justify-between mt-3">
+                    <h2 className="text-2xl mb-4 mt-2 ms-5 font-bold">ENKKU | ChatBot</h2>
+                    <button onClick={toggleSidebar} className="me-2 hover:cursor-pointer p-1 rounded-md">
+                        <span className="material-icons flex justify-center items-center m-0 pt-1.5 px-1">close</span>
+                    </button>
+                </div>
+                <ul className="mt-5 ms-5 text-xl">
+                    <li className="mb-5 hover:text-gray-400 hover:cursor-pointer">
+                        <button type="button" onClick={() => router.push("/")}>
+                            Home
+                        </button>
+                    </li>
+                    <li className="mb-5 hover:text-gray-400 hover:cursor-pointer">
+                        <button type="button" onClick={() => router.push("/about")}>
+                            About us
+                        </button>
+                    </li>
+                </ul>
+            </div>
+
+            {/* Navbar */}
+            <header className="bg-white shadow-xs z-2">
+                <div className="flex items-center justify-between p-4 mx-auto max-w-screen-xl">
+                    <button className="p-2 justify-center items-center hover:cursor-pointer" onClick={toggleSidebar}>
+                        <span className={`material-icons text-orange-600 flex justify-center items-center m-0 pt-1.5 px-1.5 ${isSidebarOpen ? "invisible" : ""
+                            }`} style={{ fontSize: "30px" }}>
+                            menu
+                        </span>
+                    </button>
+                    <div className="items-center mx-auto">
+                        <img
+                            src="https://www.en.kku.ac.th/web/wp-content/uploads/2021/06/Logo-web.png"
+                            className="h-12"
+                            alt="EN KKU Logo"
+                        />
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { useRouter } from 'next/navigation'
+import NavSidebar from "./components/NavSidebar"; // Import Component ที่สร้างใหม่
 
 interface Message {
   id: number;
@@ -9,36 +9,42 @@ interface Message {
 }
 
 export default function Home() {
-
-  // { id: 1, text: "Hi, Who are you?", sender: "user" },
-  // { id: 2, text: "This AI chatbot has been developed to...", sender: "bot" },
-
-  const router = useRouter();
   const [messages, setMessages] = useState<Message[]>([]);
   const [textInput, setTextInput] = useState<string>("");
-  var index = 0;
+  const [isLoading, setIsLoading] = useState(false);
 
   const sendMessage = () => {
     if (textInput.trim() !== "") {
       setMessages((prevMessages) => [
         ...prevMessages,
         { id: prevMessages.length + 1, text: textInput, sender: "user" },
-        { id: prevMessages.length + 2, text: "BOT_LOADING", sender: "bot" }
+        { id: prevMessages.length + 2, text: "BOT_LOADING", sender: "bot" },
       ]);
-      index = messages.length + 2;
-      console.log(index);
+      setIsLoading(true);
+      handleSubmit(textInput);
       setTextInput("");
     }
   };
 
-  const setText = (value: string) => {
-    setTextInput(value);
-  };
+  const handleSubmit = async (query: string) => {
+    try {
+      const res = await fetch("http://localhost:5000/chat", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ query }),
+      });
 
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+      const data = await res.json();
 
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
+      if (data) {
+        setMessages((prevMessages) =>
+          prevMessages.map((msg) => (msg.text === "BOT_LOADING" ? { ...msg, text: data.answer } : msg))
+        );
+        setIsLoading(false);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   const LoadingSpinner = () => (
@@ -47,130 +53,84 @@ export default function Home() {
     </div>
   );
 
-
-
   return (
     <div className="bg-gray-100">
       <div className="flex h-screen">
-        {/* Sidebar */}
-        <div
-          className={`bg-gray-800 text-white md:w-100 w-screen p-2 fixed left-0 top-0 h-full z-10 transition-transform duration-300 ${isSidebarOpen ? "translate-y-0" : "-translate-y-full"
-            }`}
-        >
-          <div className="flex justify-between mt-3">
-            <h2 className="text-2xl mb-4 mt-2 ms-5 font-bold">ENKKU | ChatBot</h2>
-            <button onClick={toggleSidebar} className={`me-2 hover:cursor-pointer p-1 border-1 rounded-md border-gray-600 ${isSidebarOpen ? "d-none" : null}`}>
-              <span className="material-icons flex justify-center items-center m-0 pt-1.5 px-1.5">
-                close
-              </span>
-            </button>
-          </div>
-          <ul className="mt-5 ms-5 text-xl">
-            <li className="mb-5 hover:text-gray-400 hover:cursor-pointer">
-              <button type="button" onClick={() => router.push('/')}>
-                Home
-              </button>
-            </li>
-            <li className="mb-5 hover:text-gray-400 hover:cursor-pointer">
-              <button type="button" onClick={() => router.push('/about')}>
-                About us
-              </button>
-            </li>
-          </ul>
-        </div>
-
         <div className="flex-1 flex flex-col">
-          {/* Navbar */}
-          <header className="bg-white shadow-xs z-2">
-            <div className="flex items-center justify-between p-4 mx-auto max-w-screen-xl">
-              <button
-                className="p-2 justify-center items-center hover:cursor-pointer"
-                onClick={toggleSidebar} // Toggle sidebar on menu click
-              >
-                <span
-                  className="material-icons pt-1 px-1 text-orange-500 font-bold"
-                  style={{ fontSize: "30px" }}
-                >
-                  menu
-                </span>
-              </button>
-              <div className="items-center mx-auto">
-                <img
-                  src="https://www.en.kku.ac.th/web/wp-content/uploads/2021/06/Logo-web.png"
-                  className="h-12"
-                  alt="EN KKU Logo"
-                />
-              </div>
-            </div>
-          </header>
-
-          {/* Chat Area */}
+          <NavSidebar />
           <main className="flex-1 overflow-y-auto p-4 bg-gray-100">
             <div className="max-w-screen-xl mx-auto">
               {messages.length !== 0 ? (
                 messages.map((msg) => (
-                  <div
-                    key={msg.id}
-                    className={`flex ${msg.sender === "user" ? "justify-end" : "justify-start"
-                      } mb-2`}
-                  >
-                    <div
-                      className={`rounded-lg flex shadow ${msg.sender === "user" ? "bg-white text-gray-800" : "bg-white text-gray-800"
-                        }`}
-                    >
-                      <div className="w-[5px] rounded-lg bg-orange-500"></div>
-                      <div className="max-w-xs p-2 inputps-4">
-                        {msg.text === "BOT_LOADING" && msg.sender === "bot" ? <div className="flex"><LoadingSpinner /><p className="ms-2">กรุณารอสักครู่</p></div> : msg.text}
-                      </div>
+                  <div key={msg.id} className={`flex ${msg.sender === "user" ? "justify-end" : "justify-start"} mb-2`}>
+                    <div className={`rounded-lg flex shadow ${msg.sender === "user" ? "text-white bg-orange-500" : "text-gray-800 bg-white"}`}>
+                      {
+                        msg.sender === "user" ? null : <div className="w-[5px] rounded-lg bg-orange-500"></div>
+                      }
+                      <div className="max-w-xs p-2">{msg.text === "BOT_LOADING" ? <div className="flex"><LoadingSpinner /><p className="ms-2 text-orange-600">กรุณารอสักครู่</p></div> : msg.text}</div>
                     </div>
                   </div>
                 ))
               ) : (
-                <div className="max-w-screen-xl mx-auto mt-[250px] h-full">
+                <div className="max-w-screen-xl mx-auto mt-[150px] h-full">
                   <div className="my-auto flex flex-col">
+                    <img className="w-[500px] mx-auto" src="/chatbot.png" alt="" />
                     <div className="text-gray-800 text-4xl text-center">Welcome</div>
-                    <p className="text-gray-800 text-xl text-center mt-2">
-                      ยินดีต้อนรับเข้าสู่ ENKKU ChatBot
-                    </p>
+                    <p className="text-gray-800 text-xl text-center mt-2">ยินดีต้อนรับเข้าสู่ ENKKU ChatBot</p>
                   </div>
-                  <div className="mt-[80px] max-w-screen-xl mx-auto border rounded-lg flex">
+                  <form
+                    className="mt-[80px] max-w-screen-xl mx-auto border rounded-lg flex"
+                    onSubmit={(event) => {
+                      event.preventDefault();
+                      sendMessage();
+                    }}
+                  >
                     <input
                       type="text"
                       placeholder="พิมพ์คำถามของคุณที่นี่"
+                      value={textInput}
+                      disabled={isLoading}
                       className="flex-1 p-2 ps-4 text-black border-gray-300 bg-gray-50 focus:outline-none"
-                      onChange={(event) => setText(event.target.value)}
+                      onChange={(event) => setTextInput(event.target.value)}
                     />
-                    <button className="p-2 bg-orange-500 hover:bg-amber-700 hover:cursor-pointer duration-500 ease-in-out text-white rounded-lg" onClick={sendMessage}>
+                    <button
+                      type="submit"
+                      className="p-2 bg-orange-500 hover:bg-amber-700 text-white rounded-lg"
+                      disabled={isLoading}
+                    >
                       <span className="material-icons pt-1 px-1">send</span>
                     </button>
-                  </div>
+                  </form>
                 </div>
               )}
             </div>
           </main>
+          {messages.length !== 0 ? <footer className="p-5 pb-5 bg-white shadow-md items-center">
+            <form
+              className="max-w-screen-xl mx-auto border rounded-lg flex"
+              onSubmit={(event) => {
+                event.preventDefault();
+                sendMessage();
+              }}
+            >
+              <input
+                type="text"
+                placeholder="พิมพ์คำถามของคุณที่นี่"
+                value={textInput}
+                disabled={isLoading}
+                className="flex-1 p-2 ps-4 text-black border-gray-300 bg-gray-50 focus:outline-none"
+                onChange={(event) => setTextInput(event.target.value)}
+              />
+              <button
+                type="submit"
+                className={`p-2 ${isLoading ? "bg-gray-600" : "bg-orange-500 hover:bg-amber-700"}  text-white rounded-lg`}
+                disabled={isLoading}
+              >
+                <span className="material-icons pt-1 px-1">{isLoading ? "stop_circle" : "send"}</span>
+              </button>
+            </form>
+          </footer> : null}
 
-          {/* Message Input */}
-          {messages.length !== 0 ? (
-            <footer className="p-5 pb-5 bg-white shadow-md items-center">
-              <div className="max-w-screen-xl mx-auto border rounded-lg flex">
-                <input
-                  type="text"
-                  placeholder="พิมพ์คำถามของคุณที่นี่"
-                  value={textInput}
-                  className="flex-1 p-2 ps-4 text-black border-gray-300 bg-gray-50 focus:outline-none"
-                  onChange={(event) => setText(event.target.value)}
-                  onKeyDown={(event) => {
-                    if (event.key === "Enter") {
-                      sendMessage();
-                    }
-                  }}
-                />
-                <button className="p-2 bg-orange-500 hover:bg-amber-700 hover:cursor-pointer duration-500 ease-in-out text-white rounded-lg" onClick={sendMessage}>
-                  <span className="material-icons pt-1 px-1">send</span>
-                </button>
-              </div>
-            </footer>
-          ) : null}
         </div>
       </div>
     </div>

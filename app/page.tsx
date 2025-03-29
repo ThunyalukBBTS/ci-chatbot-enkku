267cedec9e1ec46c04f57fac704d7ce64bab4189
thunyalukBBTS <thunyaluk.sa@kkumail.com>
2025-03-30 01:02:02 +0700

    [Fix] - Change backend ip address to azure container

--- app/page.tsx
+++ app/page.tsx
@@ -26,9 +26,11 @@ export default function Home() {
     }
   };
 
+  const backendIP: String = "20.24.166.116";
+
   const handleSubmit = async (query: string) => {
     try {
-      const res = await fetch("http://localhost:5000/chat", {
+      const res = await fetch(`http://${backendIP}:8000/chat`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ query }),
